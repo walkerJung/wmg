bf3b9c4e0254e9a6b0bb8cf64697459cc483e984
walkerJung <jjh_walker@kakao.com>
2023-03-13 22:45:32 +0900

    feat: install dependency

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">HEllo WOrld</h1>
+      <h1 className="text-5xl font-bold underline">HEllo WOrld</h1>
     </div>
   );
 }
